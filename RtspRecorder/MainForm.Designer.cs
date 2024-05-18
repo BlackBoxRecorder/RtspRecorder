8d1d4653438d2ef55477ece272d5914464a4457d
Tim Cooper <timetickme@outlook.com>
2024-05-18 13:50:11 +0800

    添加项目文件。

---
+++ RtspRecorder/MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace RtspRecorder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextAddr = new System.Windows.Forms.TextBox();
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.BtnStopPlay = new System.Windows.Forms.Button();
+            this.BtnRecord = new System.Windows.Forms.Button();
+            this.LabelRecordTime = new System.Windows.Forms.Label();
+            this.LabelVideoFile = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "视频源";
+            // 
+            // TextAddr
+            // 
+            this.TextAddr.Location = new System.Drawing.Point(84, 20);
+            this.TextAddr.Name = "TextAddr";
+            this.TextAddr.Size = new System.Drawing.Size(267, 21);
+            this.TextAddr.TabIndex = 1;
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.Location = new System.Drawing.Point(39, 57);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(75, 23);
+            this.BtnPlay.TabIndex = 2;
+            this.BtnPlay.Text = "播放";
+            this.BtnPlay.UseVisualStyleBackColor = true;
+            this.BtnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // BtnStopPlay
+            // 
+            this.BtnStopPlay.Location = new System.Drawing.Point(159, 57);
+            this.BtnStopPlay.Name = "BtnStopPlay";
+            this.BtnStopPlay.Size = new System.Drawing.Size(75, 23);
+            this.BtnStopPlay.TabIndex = 2;
+            this.BtnStopPlay.Text = "停止";
+            this.BtnStopPlay.UseVisualStyleBackColor = true;
+            this.BtnStopPlay.Click += new System.EventHandler(this.BtnStopPlay_Click);
+            // 
+            // BtnRecord
+            // 
+            this.BtnRecord.Location = new System.Drawing.Point(276, 57);
+            this.BtnRecord.Name = "BtnRecord";
+            this.BtnRecord.Size = new System.Drawing.Size(75, 23);
+            this.BtnRecord.TabIndex = 2;
+            this.BtnRecord.Text = "录制";
+            this.BtnRecord.UseVisualStyleBackColor = true;
+            this.BtnRecord.Click += new System.EventHandler(this.BtnRecord_Click);
+            // 
+            // LabelRecordTime
+            // 
+            this.LabelRecordTime.AutoSize = true;
+            this.LabelRecordTime.Location = new System.Drawing.Point(37, 100);
+            this.LabelRecordTime.Name = "LabelRecordTime";
+            this.LabelRecordTime.Size = new System.Drawing.Size(29, 12);
+            this.LabelRecordTime.TabIndex = 3;
+            this.LabelRecordTime.Text = "录制";
+            // 
+            // LabelVideoFile
+            // 
+            this.LabelVideoFile.AutoSize = true;
+            this.LabelVideoFile.Location = new System.Drawing.Point(193, 100);
+            this.LabelVideoFile.Name = "LabelVideoFile";
+            this.LabelVideoFile.Size = new System.Drawing.Size(29, 12);
+            this.LabelVideoFile.TabIndex = 3;
+            this.LabelVideoFile.Text = "文件";
+            this.LabelVideoFile.Click += new System.EventHandler(this.LabelVideoFile_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 136);
+            this.Controls.Add(this.LabelVideoFile);
+            this.Controls.Add(this.LabelRecordTime);
+            this.Controls.Add(this.BtnRecord);
+            this.Controls.Add(this.BtnStopPlay);
+            this.Controls.Add(this.BtnPlay);
+            this.Controls.Add(this.TextAddr);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RtspRecorder";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextAddr;
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.Button BtnStopPlay;
+        private System.Windows.Forms.Button BtnRecord;
+        private System.Windows.Forms.Label LabelRecordTime;
+        private System.Windows.Forms.Label LabelVideoFile;
+    }
+}
+
---
+++ RtspRecorder/MainForm.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace RtspRecorder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private CancellationTokenSource cts;
+        private CancellationToken token;
+
+        private readonly string myVideoDir = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.MyVideos),
+            "CameraView"
+        );
+
+        private readonly ConcurrentQueue<Mat> queue = new ConcurrentQueue<Mat>();
+        private bool isRecording = false;
+        private bool isPlaying = false;
+        private string videoFullpath;
+        private double fps;
+        private int videoWidth;
+        private int videoHeight;
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            VideoCapture cap = new VideoCapture();
+            var addr = TextAddr.Text;
+            var suc = int.TryParse(addr, out var camIndex);
+            BtnPlay.Enabled = false;
+
+            if (suc)
+            {
+                cap.Open(camIndex);
+            }
+            else
+            {
+                cap.Open(addr);
+            }
+
+            if (!cap.IsOpened())
+            {
+                MessageBox.Show("视频源打开失败");
+                BtnPlay.Enabled = true;
+                return;
+            }
+
+            fps = cap.Fps;
+            videoWidth = cap.FrameWidth;
+            videoHeight = cap.FrameHeight;
+
+            if (cts == null)
+            {
+                cts = new CancellationTokenSource();
+                token = cts.Token;
+            }
+
+            while (!token.IsCancellationRequested)
+            {
+                var mat = new Mat();
+                var readSuccess = cap.Read(mat);
+                if (!readSuccess)
+                {
+                    MessageBox.Show("视频结束");
+                    CancelToken();
+                    BtnPlay.Enabled = true;
+                    return;
+                }
+
+                if (isRecording)
+                {
+                    queue.Enqueue(mat);
+                }
+
+                Cv2.ImShow(addr, mat);
+                Cv2.WaitKey(5);
+
+                isPlaying = true;
+            }
+
+            Cv2.DestroyAllWindows();
+            cap.Release();
+            cap.Dispose();
+            BtnPlay.Enabled = true;
+            isPlaying = false;
+        }
+
+        private void BtnStopPlay_Click(object sender, EventArgs e)
+        {
+            CancelToken();
+        }
+
+        private void CancelToken()
+        {
+            isRecording = false;
+            isPlaying = false;
+            if (cts != null)
+            {
+                cts.Cancel();
+                cts.Dispose();
+                cts = null;
+            }
+        }
+
+        private async void BtnRecord_Click(object sender, EventArgs e)
+        {
+            if (!isPlaying)
+            {
+                MessageBox.Show("请先播放视频，再开始录制");
+                return;
+            }
+            VideoWriter writer = new VideoWriter();
+            videoFullpath = GetVideoFileName();
+
+            BtnRecord.Enabled = false;
+            bool opened = writer.Open(
+                videoFullpath,
+                FourCC.H264,
+                fps,
+                new OpenCvSharp.Size(videoWidth, videoHeight)
+            );
+
+            if (!opened)
+            {
+                MessageBox.Show("视频写入失败");
+                BtnRecord.Enabled = true;
+                return;
+            }
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+            isRecording = true;
+
+            while (!token.IsCancellationRequested)
+            {
+                if (queue.Count < 1)
+                {
+                    await Task.Delay(1);
+                    continue;
+                }
+
+                if (queue.TryDequeue(out Mat tmpMat))
+                {
+                    writer.Write(tmpMat);
+                }
+
+                LabelRecordTime.Text = $"录制：{stopwatch.Elapsed:hh':'mm':'ss}";
+            }
+
+            BtnRecord.Enabled = true;
+
+            writer.Release();
+            writer.Dispose();
+            stopwatch.Stop();
+
+            LabelVideoFile.Text = $"文件：{Path.GetFileName(videoFullpath)}";
+        }
+
+        private string GetVideoFileName()
+        {
+            var hashId = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            var videoFile = $"{hashId}.mp4";
+            var videoFullpath = Path.Combine(myVideoDir, videoFile);
+
+            if (!Directory.Exists(myVideoDir))
+            {
+                Directory.CreateDirectory(myVideoDir);
+            }
+
+            return videoFullpath;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isRecording || isPlaying)
+            {
+                e.Cancel = true;
+                MessageBox.Show("请停止录制");
+            }
+        }
+
+        private void LabelVideoFile_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(videoFullpath))
+            {
+                return;
+            }
+
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "explorer.exe",
+                    Arguments = Path.GetDirectoryName(videoFullpath),
+                    UseShellExecute = false
+                };
+                Process.Start(startInfo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
